171a949d7a9292d4d998c848c917a5c9690cdd7c
atjeprahmat <atjeprahmat@gmail.com>
2020-01-19 10:42:20 +0700

    upload_pertama

---
+++ modul5.rb
@@ -0,0 +1,21 @@
+=begin
+    
+rescue => exception
+    
+end
+puts "Nama 1 : "
+nama1 = gets.chomp #ambil string
+
+puts "Nama 2 : "
+nama2 = gets.chomp #ambil string
+
+puts "Hasilnya #{nama1 + nama2}"
+puts nama1 + " " + nama2
+=end
+
+nama = "Atjep"
+umur = '29'
+
+puts "Nama saya #{nama}, dan umur saya #{umur} tahun"
+
+puts 'Nama saya #{nama}, dan umur saya #{umur} tahun'
---
+++ modul6.rb
@@ -0,0 +1,29 @@
+=begin
+bil1 = 10.0 #kalo float tinggal tambah . maka hasilnya ada . salah satu aja sih
+bil2 = 5
+
+hasil_penjumlahan = bil1 + bil2
+hasil_pengurangan = bil1 - bil2
+hasil_pembagian = bil1 / bil2
+hasil_perkalian = bil1 * bil2
+
+puts "penjumlahan #{hasil_penjumlahan}"
+puts "pengurangan #{hasil_pengurangan}"
+puts "pembagian #{hasil_pembagian}"
+puts "perkalian #{hasil_perkalian}"
+=end
+
+bil1 = 10 #kalo float tinggal tambah . maka hasilnya ada . salah satu aja sih
+bil2 = 5
+
+hasil_penjumlahan = bil1 + bil2
+hasil_pengurangan = bil1 - bil2
+hasil_pembagian = bil1 / bil2
+hasil_perkalian = bil1 * bil2
+hasil_pemangkatan = bil1 ** bil2
+
+puts "penjumlahan #{hasil_penjumlahan}"
+puts "pengurangan #{hasil_pengurangan}"
+puts "pembagian #{hasil_pembagian}"
+puts "perkalian #{hasil_perkalian}"
+puts "pemangkatan #{hasil_pemangkatan}"
