b701b726d88a5083a6ba126298b343590798adc3
atjeprahmat <atjeprahmat@gmail.com>
2020-01-19 13:57:52 +0700

    13

---
+++ modul13.rb
@@ -0,0 +1,23 @@
+=begin
+boolean menunjukan nilai true atau false
+operator pembanding : >, <, >=, <=, ==, !=
+operator logika : &&, ||, !
+=end
+
+puts "---------------------------"
+puts 90 == 90
+puts 90 > 80
+puts "---------------------------"
+puts true && true
+puts true && false
+puts false && true
+puts false && false
+puts "---------------------------"
+puts true || true
+puts true || false
+puts false || true
+puts false || false
+puts "---------------------------"
+puts !true
+puts !false
+puts !!!!!!!!!!true
