571810efabfce445def49366cac5d1a5b7a66401
atjeprahmat <atjeprahmat@gmail.com>
2020-01-19 13:40:13 +0700

    11

--- modul11.rb
+++ modul11.rb
@@ -4,9 +4,20 @@
 end
 =end
 
-
+=begin
 3.times {
     puts "Selamat Datang"
     puts "Pemrograman"
     puts "Ruby"
 }
+=end
+
+5.times do |nomor|
+    puts "Selamat datang di Pemrograman Ruby #{nomor}"
+end
+
+=begin
+5.times do |nomor|
+    puts "Selamat datang di Pemrograman Ruby #{nomor+1}"
+end
+=end
