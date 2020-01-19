9c45e0b0d11c109277b6961f91860f53f5068dd2
atjeprahmat <atjeprahmat@gmail.com>
2020-01-19 14:06:42 +0700

    14

---
+++ modul14.rb
@@ -0,0 +1,21 @@
+=begin
+nilai = 55
+if nilai >= 70
+    puts 'Kamu Kompeten'
+else
+    puts 'Kamu Belum Kompeten'
+end
+=end
+
+
+password = 'admin123 '
+if password == 'admin123'
+    puts 'Kamu berhasil login'
+    puts 'dan silakan menggunakan internet'
+else
+    puts 'Kamu gagal login bro'
+end
+
+puts 'Kamu berhasil login' if password == 'admin123' #hanya 1 perintah yang berjalan
+
+
