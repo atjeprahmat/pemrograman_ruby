63e989c1b763a7ba49a86fea3bd0111822684010
atjeprahmat <atjeprahmat@gmail.com>
2020-01-19 15:25:57 +0700

    25

--- modul25.rb
+++ modul25.rb
@@ -7,9 +7,17 @@ kirim
 
 puts "-------------------"
 
-def kirim2 (email2='atjeprahmat@gmail.com', pesan2='Nasi Goreng')
-    puts "Email : #{email2}"
+def kirim2 (email2='atjeprahmat@gmail.com', pesan2)
+    puts email2
     puts "Pesanana : #{pesan2}"
 end
 
-kirim2
+kirim2('Ayam Bakar')
+
+puts "-------------------"
+def kirim3 (email3, pesan3)
+    puts "Email anda : #{email3}"
+    puts "Pesanana : #{pesan3}"
+end
+
+kirim3("atjeprahmat@gmail.com", "Ayam Bakar")
