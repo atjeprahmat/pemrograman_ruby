f5e433e0c283fab569ffbf276a1228ec5478caa1
atjeprahmat <atjeprahmat@gmail.com>
2020-01-19 11:16:51 +0700

    upload_pertama

---
+++ modul7.rb
@@ -0,0 +1,14 @@
+#PEMDAS
+#Parentheses ()
+#Eksponential **
+#Multiplication *
+#Devision /
+#Addition +
+#substraction -
+
+puts 1-2+3*4 #spasi gak pengaruh, kali dlu 3*4, lalu 1-2 hasilnya -1 + 12
+
+puts 1+2-3/4 #1 + 2 dlu terus 3/4 lalu tambahkan keduanya
+puts (1+2)-(3/4)
+puts 1 * (2-3)/4
+puts 1 ** 2 - (3/4)
---
+++ modul8.rb
@@ -0,0 +1,28 @@
+#penggunaan methode untuk merubah string
+
+=begin
+puts "wikrama".upcase
+puts "WIKRAMA".downcase
+puts "WikRama".downcase
+=end
+
+=begin
+nama = "WIKRAMA"
+puts nama.downcase
+=end
+
+=begin
+nama = "WIKRAMA"
+puts nama.downcase! #pake tanda seru biar permanen, kalo gk pake akan kembali k awal
+
+nama
+=end
+
+nama = "Atjep"
+sekolah = "Wikrama
+"
+puts nama.capitalize!
+sekolah.upcase
+nama
+
+puts "Nama Saya #{nama}, dan saya sekolah di #{sekolah}"
