29274d1fdfc572b33641beaa405880e5d88e4ee3
atjeprahmat <atjeprahmat@gmail.com>
2020-01-19 13:48:31 +0700

    11_2

---
+++ modul12.rb
@@ -0,0 +1,15 @@
+=begin
+4.upto(9).each do |nomor|
+    puts "#{nomor}"
+    puts "Selamat Belajar"
+    puts 'Pemrograman Ruby'
+end
+=end
+
+4.upto(9).each do |nomor|
+    puts "#{nomor}"
+    puts "Selamat Belajar"
+    puts 'Pemrograman Ruby'
+end
+
+puts "Nama saya Atjep".sub 'Atjep', 'Rahmat'
